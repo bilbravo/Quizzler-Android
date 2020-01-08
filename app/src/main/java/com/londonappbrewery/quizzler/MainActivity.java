d4661b377b0a8ba7030aa65f04e175fc22dd4cb5
bilbravo <bilbravo@gmail.com>
2020-01-08 13:50:35 -0500

    created TrueFalse class

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -2,6 +2,10 @@ package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,7 +13,8 @@ public class MainActivity extends Activity {
 
 
     // TODO: Declare member variables here:
-
+    Button mTrueButton;
+    Button mFalseButton;
 
     // TODO: Uncomment to create question bank
 //    private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -32,6 +37,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        View.OnClickListener myListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Quizzler", "Button pressed!");
+                Toast.makeText(getApplicationContext(), "True pressed!", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        mTrueButton.setOnClickListener(myListener);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast myToast = Toast.makeText( getApplicationContext(), "False pressed!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
 
 
 
